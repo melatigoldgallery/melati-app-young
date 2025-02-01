ceb260b439ce5dc922d403076b640534f3254168
unknown <fattahula98@gmail.com>
2025-02-01 13:46:07 +0800

    notif setiap 1 menit

--- js/audioHandlers.js
+++ js/audioHandlers.js
@@ -71,8 +71,8 @@ export function announceQueueNumber(queueNumber, language = 'id') {
   const numbers = queueNumber.substring(1);
 
   const texts = {
-    id: `Nomor antrian, ${letter} ${numbers.split("").join("")}`,
-    en: `Queue number, ${letter} ${numbers.split("").join("")}`
+    id: `Nomor antrian, ${letter} ${numbers.split("").join(" ")}`,
+    en: `Queue number, ${letter} ${numbers.split("").join(" ")}`
   };
 
   const utterance = new SpeechSynthesisUtterance(texts[language]);
@@ -157,7 +157,7 @@ export async function announceVehicleMessage(carType, plateNumber, language = 'i
   window.speechSynthesis.cancel();
 }
 
-// export function playNotificationSound() {
-//   const notifSound = new Audio(AUDIO_PATHS.notifOn);
-//   notifSound.play();
-// }
+export function playNotificationSound() {
+  const notifSound = new Audio(AUDIO_PATHS.notifOn);
+  notifSound.play();
+}
